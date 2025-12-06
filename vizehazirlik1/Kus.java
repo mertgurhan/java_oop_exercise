fa2d675abd8cddb06e13ac6d7bde5fc1e1d9559a
Mert Gürhan <103640237+mertgurhan@users.noreply.github.com>
2025-12-06 14:12:18 +0300

    Add files via upload

---
+++ vizehazirlik1/Kus.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vizehazirlik1;
+
+/**
+ *
+ * @author mertgurhan
+ */
+
+public class Kus implements SesliHayvan,UcanHayvan {
+    @Override
+    public void SesCikar(){
+        System.out.println("cik cik");
+    }
+    @Override
+    public void Uc(){
+        System.out.println("Kus ucuyor");
+    }
+
+
+    
+}
